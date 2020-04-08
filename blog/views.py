87a7719e0b9c7ebb0e2412c61e0eeb3abc40f222
Chuckz Okoye <tricelex@gmail.com>
2020-04-08 16:15:34 +0100

    created views for share post module

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from blog.models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -30,3 +31,15 @@ def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
 							 publish__month=month, publish__day=day)
 	return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+	post = get_object_or_404(Post, id=post_id, status='published')
+	if request.method == 'POST':
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+		# TODO: Send mail
+	else:
+		form = EmailPostForm()
+		return render(request, 'blog/post/share.html', {'post': post, 'form': form})
